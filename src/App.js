e7c0995d59960164ccae0e56c5321e658aeec6e6
krishnkantiscoding <mail2krishnkant@gmail.com>
2022-03-08 17:36:58 +0530

    firstpush

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import photo from './kk.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="pic" src={photo} /> 
+        <div className="About">
+          <h1>I am Krishn Kant Shukla</h1>
+          <h2>Currently persuing my Bachelors<br/>
+          in Computer Applications with specialization in Data Science and Artificial Intellegence<br/>from BBD University.
+          </h2>
+          <h3>Email: mail2krishnkant@gmail.com</h3>
+        </div>
+
+        <div className="project">
+        <h1>Projects</h1>
+          <li>GitHub Notifier : Listen for events from GitHub and notify you.</li><br/>
+          <li>Twitter Bot : Build a program that submits new status messages to Twitter.</li><br/>
+          <li>Weather API : Use the Forecast.io api to display the weather near you.</li>
+        
+        </div>
+        <div className="Skills">
+          <h1>Skills</h1>
+      <p>React</p>
+      <div class="container">
+      <div class=" react">90%</div>
+      </div>
+
+      <p>JavaScript</p>
+      <div class="container">
+      <div class=" js ">70%</div>
+     </div>
+
+     <p>HTML</p>
+      <div class="container">
+      <div class=" html ">90%</div>
+     </div>
+
+     <p>C++</p>
+      <div class="container">
+      <div class="cpp ">75%</div>
+     </div>
+
+     <p>Python</p>
+      <div class="container">
+      <div class=" python">95%</div>
+     </div>
+      </div>
+       
     </div>
   );
 }
